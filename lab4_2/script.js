919d979b7a52c7813354f77da32e9056992b60f4
Christina dt <cnicodc@gmail.com>
2023-03-07 08:27:10 -0800

    updated lab 4.2 and new images for photo gallery

---
+++ lab4_2/script.js
@@ -0,0 +1,33 @@
+const baseURL = '/ARTDM174/images/';
+const nxt = document.querySelector('.nxt');
+const prev = document.querySelector('.prev');
+const slide = document.querySelector('.pic');
+const cats = ['tomasina.jpg', 'seuss.jpg', 'felix.jpg', 'pinoccio.jpg', 'cheshire.jpg'];
+let index = 0;
+
+nxt.onclick = function(e) {
+  e.preventDefault();
+  
+  slide.src = baseURL + cats[index];
+  index++;
+  console.log(index);
+ 
+  //if end of array, start at beginning [0] again
+  if(index >= cats.length) {
+    index = 0;
+  }
+}
+
+//Previous button event listener
+prev.onclick = function(e) {
+  e.preventDefault();
+  if (index === 0){
+    index = cats.length - 1;
+  }
+  else {
+    index--;
+  }
+  slide.src = baseURL + cats[index];
+  console.log(index);
+}
+
